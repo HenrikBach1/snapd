954bf05db5fb1408c400e2f0ad66c25e1d3eb053
Henrik Bach <bach.henrik@gmail.com>
2022-01-30 15:48:16 +0000

    Abuilding

--- .devcontainer/scripts/docker-create-consumer-image.sh
+++ .devcontainer/scripts/docker-create-consumer-image.sh
@@ -7,5 +7,6 @@ IMAGE_FILTER_UID=${IMAGE_BASE_NAME}'-*-uid:'${IMAGE_TAG}
 IMAGE_FILTER=${IMAGE_BASE_NAME}'*'
 IMAGE_ID=$(docker images --filter=reference=${IMAGE_FILTER_UID} | grep -iv image | awk '{ print $3 }')
 
+echo ${IMAGE_ID}
 docker tag ${IMAGE_ID} ${IMAGE_REPO_NAME_NEW}
 docker images --filter=reference=${IMAGE_FILTER}
